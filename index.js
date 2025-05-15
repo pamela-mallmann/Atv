5e38fc51be99a92602d897384955575adb814293
Pamela Mallmann <pamelamallmanncomputador@gmail.com>
2025-05-15 16:56:50 -0300

    Add files via upload
    Adicionando site completo

---
+++ index.js
@@ -0,0 +1,5 @@
+function exibirMensagem() { //função ao exibir mensagem//
+  const mensagem = document.getElementById("mensagem"); 
+  mensagem.textContent = "GIRLS WHO CODE WRITE THE FUTURE"; //frase exibida
+  mensagem.style.display = "block";
+}
